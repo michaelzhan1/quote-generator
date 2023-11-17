5fcdc52f733bc2de705aaa70a9446029dabfabc1
Michael Zhan <michaelzhan2001@gmail.com>
2023-11-17 12:31:48 -0500

    Set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
