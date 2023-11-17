b0b458318a1a9546a4c89aab6657625dcecd1595
Michael Zhan <michaelzhan2001@gmail.com>
2023-11-17 12:50:51 -0500

    Fix footer tailwind

--- app/Footer.js
+++ app/Footer.js
@@ -1,8 +1,3 @@
-'use client'
-
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
@@ -11,17 +6,17 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 export default function Footer({ imageAuthor, imageAuthorLink }) {
   return (
     <>
-      <Container fluid className='p-0 fixed-bottom d-flex justify-content-between' style={{backgroundColor: 'darkslategray', color: 'white'}}>
+      <div className='fixed bottom-0 flex justify-between bg-slate-700 text-white'>
         <div className='ms-3 my-2 text-start'>
           <span>
             Connect:
-            <a href='mailto:michaelzhan2001@gmail.com' target='_blank' rel='noreferrer' className='ms-1' style={{color: 'white'}}>
+            <a href='mailto:michaelzhan2001@gmail.com' target='_blank' rel='noreferrer' className='ms-1 text-white'>
               <FontAwesomeIcon icon={faEnvelope} />
             </a>
-            <a href='https://linkedin.com/in/michaelzhan1' target='_blank' rel='noreferrer' className='ms-1' style={{color: 'white'}}>
+            <a href='https://linkedin.com/in/michaelzhan1' target='_blank' rel='noreferrer' className='ms-1 text-white'>
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
-            <a href='https://github.com/michaelzhan1' target='_blank' rel='noreferrer' className='ms-1' style={{color: 'white'}}>
+            <a href='https://github.com/michaelzhan1' target='_blank' rel='noreferrer' className='ms-1 text-white'>
               <FontAwesomeIcon icon={faGithub} />
             </a>
           </span>
@@ -29,12 +24,12 @@ export default function Footer({ imageAuthor, imageAuthorLink }) {
         <div className='me-3 my-2 text-end'>
           <span>
             Photo by
-            <a href={imageAuthorLink} target='_blank' rel='noreferrer' className='ms-1' style={{color: 'white'}}>{imageAuthor}</a>
+            <a href={imageAuthorLink} target='_blank' rel='noreferrer' className='ms-1 text-white'>{imageAuthor}</a>
             &nbsp;on
-            <a href='https://unsplash.com/' target='_blank' rel='noreferrer' className='ms-1' style={{color: 'white'}}>Unsplash</a>
+            <a href='https://unsplash.com/' target='_blank' rel='noreferrer' className='ms-1 text-white'>Unsplash</a>
           </span>
         </div>
-      </Container>
+      </div>
     </>
   )
 }
