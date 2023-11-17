4aadca1639c7efdefce615962de8fa2c351c2ddc
Michael Zhan <michaelzhan2001@gmail.com>
2023-11-17 11:59:55 -0500

    Fix quote

--- utils/api.js
+++ utils/api.js
@@ -1,10 +1,10 @@
 export async function fetchQuote(queryParams) {
   const queryString = new URLSearchParams(queryParams).toString()
-  const response = await fetch(`https://api.api-ninjas.com/v1/quotes?${queryString}&limit=1`, {
+  const response = await fetch(`https://api.api-ninjas.com/v1/quotes?${queryString}`, {
       headers: {
         'X-Api-Key': process.env.QUOTE_API_KEY,
       },
-      cache: 'no-store',
+      cache: 'no-cache',
   });
   const data = await response.json()
   return data;
